3729e028e52f9fb3e68fc82d0c5fdecac60ffda4
gralmeidan <gralmeidan@gmail.com>
2022-07-17 17:59:32 -0400

    Added transition from 3 dashes to close icon

--- src/lib/MobileNav.svelte
+++ src/lib/MobileNav.svelte
@@ -1,6 +1,6 @@
 <script context="module">
   export const navView = tweened(0, {
-    duration: 400,
+    duration: 300,
     easing: cubicOut,
   });
 </script>
@@ -16,30 +16,40 @@
   ];
 
   const toggleVisible = () => {
-    if ($navView > 0) navView.set(0);
-    else navView.set(200);
+    if ($navView > 0) {
+      navView.set(0);
+      closeIcon = false;
+    } else {
+      navView.set(200);
+      closeIcon = true;
+    }
   };
+
+  let closeIcon = false;
 </script>
 
 <svelte:body class="overflow-hidden" />
 
 <button
   style:right={`${$navView}px`}
-  class="absolute top-0 p-2 right-0 flex flex-col gap-[6px] z-10 bg-neutral-900"
+  class="absolute top-0 p-2 right-0 flex flex-col gap-[6px] z-10 bg-neutral-900 no-highlight pt-3"
   on:click={toggleVisible}
 >
   {#each Array(3) as _}
-    <hr class="bg-white border-[1px] w-7" />
+    <hr
+      class="bg-white border-[1px] w-7 transition-all duration-200"
+      class:close-icon={closeIcon}
+    />
   {/each}
 </button>
 
 <nav
   style:width={`${$navView}px`}
-  class="overflow-hidden right-0 absolute h-[100vh] bg-neutral-900"
+  class="overflow-hidden right-0 absolute h-[100vh] inset-shadow bg-neutral-800"
 >
   {#each nav as { text, href }}
     <a
-      class="font-roboto text-white block small-caps font-semibold text-lg hover:text-pink-600 transition-colors p-2 pl-4 whitespace-nowrap "
+      class="font-roboto text-white block small-caps font-semibold text-lg hover:text-pink-600 transition-colors p-2 pl-6 whitespace-nowrap "
       {href}
     >
       {text}
@@ -49,7 +59,18 @@
 
 <style>
   .inset-shadow {
-    -webkit-box-shadow: inset 10px 1px 20px 2px rgba(0, 0, 0, 0.63);
-    box-shadow: inset 10px 1px 20px 2px rgba(0, 0, 0, 0.63);
+    -webkit-box-shadow: inset 10px 1px 20px 2px rgba(202, 155, 155, 0.63);
+    box-shadow: inset 10px 1px 20px 2px rgba(14, 14, 14, 0.404);
+  }
+  .close-icon:first-child {
+    @apply rotate-45 translate-y-2;
+  }
+
+  .close-icon:last-child {
+    @apply -rotate-45 -translate-y-2;
+  }
+
+  .close-icon:nth-child(2) {
+    @apply -translate-y-3 opacity-0;
   }
 </style>
