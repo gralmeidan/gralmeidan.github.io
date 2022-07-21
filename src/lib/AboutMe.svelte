0adbb63c57147b416ae861df6522a6f795b565a6
gralmeidan <gralmeidan@gmail.com>
2022-07-21 17:09:20 -0400

    Added skills bars to AboutMe

--- src/lib/AboutMe.svelte
+++ src/lib/AboutMe.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onDestroy } from 'svelte';
+  import SkillProgress from './SkillProgress.svelte';
 
   let age = 18;
   const ageInterval = setInterval(() => {
@@ -12,24 +13,46 @@
   onDestroy(() => {
     clearInterval(ageInterval);
   });
+
+  const skills = [
+    { name: 'JavaScript', value: 85, color: '#f7df1e' },
+    { name: 'React', value: 70, color: '#61dafb' },
+    { name: 'Tailwind', value: 60, color: '#07b6d5' },
+    { name: 'Svelte', value: 20, color: '#ff3e00' },
+  ];
 </script>
 
-<div>
-  <span>{'<about>'}</span>
+<div class="m-2 font-roboto">
   <article>
-    <h1>About me</h1>
-    <p>
-      Hey! My name is Geraldo Almeida, I am <code>{age}</code> years old, and I enjoy
-      writing code. My first contact with the wonderful world of programming was
-      making simple Discord bots with Python, and my first project with web development
-      was an extracurricular project in high school consisting of building a mobile
-      app with React Native.
-    </p>
-    <p>
-      I greatly enjoyed both experiences and have since worked towards becoming
-      a Full Stack Web Developer by enrolling in a bootcamp and doing various
-      projects to solidify my skills.
-    </p>
+    <h1 class="font-semibold text-4xl text-rose-600 text-center">About me.</h1>
+    <section class="m-4">
+      <p>
+        Hey! My name is Geraldo Almeida, I am <code>{age}</code> years old, and I
+        enjoy writing code. My first contact with the wonderful world of programming
+        was making simple Discord bots with Python, and my first project with web
+        development was an extracurricular project in high school consisting of building
+        a mobile app with React Native.
+      </p>
+      <p>
+        I greatly enjoyed both experiences and have since worked towards
+        becoming a Full Stack Web Developer by enrolling in a bootcamp and doing
+        various projects to solidify my skills.
+      </p>
+    </section>
+    <section class="border rounded-md p-2 border-neutral-600 bg-zinc-900">
+      <h2 class="text-center font-semibold">My skills</h2>
+      {#each skills as { name, value, color }}
+        <SkillProgress {name} {value} {color} />
+      {/each}
+    </section>
   </article>
-  <span>{'</about>'}</span>
 </div>
+
+<style>
+  p {
+    @apply my-2 text-center;
+  }
+  code {
+    @apply bg-neutral-800 px-1 py-[2px] text-sm rounded-md font-mono text-neutral-300;
+  }
+</style>
---
+++ src/lib/SkillProgress.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { tweened } from 'svelte/motion';
+  import { onMount } from 'svelte';
+  import cssVariables from '../hooks/cssVariables';
+
+  export let value = 0;
+  export let name;
+  export let color;
+
+  const prog = tweened(0);
+
+  onMount(() => {
+    $prog = value;
+  });
+</script>
+
+<label class="block">
+  {name} <br class="sm:hidden" />
+  <progress use:cssVariables={{ color }} max="100" value={$prog} class="" />
+</label>
+
+<style>
+  progress {
+    @apply w-full;
+  }
+  progress::-webkit-progress-bar {
+    @apply rounded-md overflow-hidden bg-neutral-500;
+  }
+  progress::-webkit-progress-value {
+    background-color: var(--color);
+  }
+</style>
