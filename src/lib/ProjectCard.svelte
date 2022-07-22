46fa0220f11304d9ab8ceb61bef4413f194a1ce3
gralmeidan <gralmeidan@gmail.com>
2022-07-22 16:50:47 -0400

    Added initial project acrd

--- src/lib/ProjectCard.svelte
+++ src/lib/ProjectCard.svelte
@@ -6,7 +6,29 @@
   export let trybe;
 </script>
 
-<div>
-  <h2>{title}</h2>
-  <p>{description['en-US']}</p>
+<div class="border border-neutral-800 rounded-md m-2 p-3 max-w-md font-roboto">
+  <h2 class="text-rose-600 font-semibold text-lg">{title}</h2>
+  <p class="text-sm my-2">{description['en-US']}</p>
+  <div class="flex gap-1">
+    {#each tags as tag}
+      <img
+        src={`./icons/${tag}.${
+          tag === 'react-testing-library' ? 'png' : 'svg'
+        }`}
+        alt={tag}
+        class="text-rose-600 w-5 mr-1"
+      />
+    {/each}
+  </div>
 </div>
+
+<style>
+  img {
+    filter: invert(8%) sepia(17%) saturate(0%) brightness(97%) contrast(84%);
+    @apply transition-all duration-200;
+  }
+
+  img:hover {
+    filter: invert(0%) sepia(0%) saturate(100%) brightness(100%) contrast(100%);
+  }
+</style>
