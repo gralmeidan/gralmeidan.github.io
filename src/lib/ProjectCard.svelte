c52567058b62fc1ec60db348d4f834e396a6e965
gralmeidan <gralmeidan@gmail.com>
2022-07-22 15:53:15 -0400

    Added tech icons

---
+++ src/lib/ProjectCard.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let description;
+  export let repository;
+  export let tags;
+  export let title;
+  export let trybe;
+</script>
+
+<div>
+  <h2>{title}</h2>
+  <p>{description['en-US']}</p>
+</div>
