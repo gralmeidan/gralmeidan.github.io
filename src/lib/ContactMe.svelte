2c433f12149d42d05d743d5f674fff0a4b2f0c63
gralmeidan <gralmeidan@gmail.com>
2022-07-24 13:49:29 -0400

    Added submit functionality to form

--- src/lib/ContactMe.svelte
+++ src/lib/ContactMe.svelte
@@ -3,10 +3,49 @@
   let name = '';
   let email = '';
   let message = '';
+  let response;
+
+  const submitForm = () => {
+    const URL = 'https://formsubmit.co/ajax/f9c030aaad6b5efec769c33a6a946cb3';
+    fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        message,
+        email,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => (response = data))
+      .catch((error) => (response = error));
+
+    name = '';
+    email = '';
+    message = '';
+  };
 </script>
 
 <section class="m-4">
-  <FancyInput type="input" name="name" value={name} label="Name" />
-  <FancyInput type="input" name="email" value={email} label="E-mail" />
-  <FancyInput type="textarea" name="message" value={message} label="Message" />
+  <form>
+    <FancyInput type="input" name="name" bind:value={name} label="Name" />
+    <FancyInput type="input" name="email" bind:value={email} label="E-mail" />
+    <FancyInput
+      type="textarea"
+      name="message"
+      bind:value={message}
+      label="Message"
+    />
+    <div class="flex">
+      <button
+        on:click|preventDefault={submitForm}
+        class="font-roboto p-2 mt-4 bg-neutral-800 px-4 rounded transition-colors hover:bg-neutral-700 active:bg-neutral-900"
+        >Submit</button
+      >
+      <p>Form submitted</p>
+    </div>
+  </form>
 </section>
