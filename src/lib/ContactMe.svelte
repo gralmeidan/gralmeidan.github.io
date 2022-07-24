15691aebae700b85fcfa3c483e77f7ad2c7b5fa1
gralmeidan <gralmeidan@gmail.com>
2022-07-24 12:15:38 -0400

    Added contactMe form

---
+++ src/lib/ContactMe.svelte
@@ -0,0 +1,12 @@
+<script>
+  import FancyInput from './FancyInput.svelte';
+  let name = '';
+  let email = '';
+  let message = '';
+</script>
+
+<section class="m-4">
+  <FancyInput type="input" name="name" value={name} label="Name" />
+  <FancyInput type="input" name="email" value={email} label="E-mail" />
+  <FancyInput type="textarea" name="message" value={message} label="Message" />
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from 'svelte';
   import AboutMe from '../lib/AboutMe.svelte';
+  import ContactMe from '../lib/ContactMe.svelte';
   import Hero from '../lib/Hero.svelte';
   import MyWorks from '../lib/MyWorks.svelte';
   let ready = false;
@@ -13,5 +14,7 @@
   <AboutMe />
   <div class="h-[20vh]" />
   <MyWorks />
+  <div class="h-[20vh]" />
+  <ContactMe />
   <div class="h-[20vh]" />
 {/if}
