a1b4d5b40e22a2460dadd00130fe79f30a37e145
gralmeidan <gralmeidan@gmail.com>
2025-05-28 19:18:26 -0400

    chore: update gh-pages to use ssh

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ publish(
   'build', // path to public directory
   {
     branch: 'gh-pages',
-    repo: 'https://github.com/gralmeidan/gralmeidan.github.io', // Update to point to your repository
+    repo: 'git@github.com:gralmeidan/gralmeidan.github.io.git', // Update to point to your repository
     user: {
       name: 'gralmeidan', // update to use your name
       email: 'gralmeidan@gmail.com', // Update to use your email
